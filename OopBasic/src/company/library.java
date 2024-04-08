b2f5bfb4337dc1dfa60648705d61d9c8ede7c7ba
dir-FY24gotohikaru <hikaru.goto.e884@dirbato.co.jp>
2024-04-08 16:34:34 +0900

    文字列について２

--- OopBasic/src/company/library.java
+++ OopBasic/src/company/library.java
@@ -6,18 +6,23 @@ public class library {
      // length()メソッド
         String str = " Hello World! ";
         System.out.println(str.length()); // 14
+        //空白も含めた半角文字数を返す
 
         // substring()メソッド
         System.out.println(str.substring(1, 4)); // "Hel"
+        //半角１文字目から４文字目までを返す
 
         // contains()メソッド
         System.out.println(str.contains("llo")); // true
+        //文字列に「llo」が含まれていたら「true」を返す
 
         // replace()メソッド
         System.out.println(str.replace('l', 'L')); // " HeLLo WorLd! "
+        //小文字の「l」を大文字の「L」に置換する
 
         // trim()メソッド
         System.out.println(str.trim()); // "Hello World!"
+        //先頭と末尾の空白を削除して表記する
 
         // formatted()メソッド
         String name = "Alice";
@@ -26,6 +31,7 @@ public class library {
 
         String formattedString = "Name: %s, Age: %d, Height: %.2f".formatted(name, age, height);
         System.out.println(formattedString);
+        //なぜ「.formatted」の形になるのか分からないので質問すること！
     }
 
 }
